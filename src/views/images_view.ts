22c2d4af68ceadd467c9836111dd2abbc1fcca23
cledson <cledman@gmail.com>
2020-10-19 09:52:55 -0300

    changes on configs

--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -1,14 +1,14 @@
-import Image from '../models/Image';
+import Image from "../models/Image";
+
 export default {
-    render(image:Image){
-        return {
-            id: image.id,
-               url: `${process.env.API_URL}/uploads/${image.path}`
+  render(image: Image) {
+    return {
+      id: image.id,
+      url: `${process.env.API_URL}/uploads/${image.path}`
+    };
+  },
 
-        }
-    },
-
-    renderMany(images: Image[]){
-        return images.map(image =>this.render(image));
-    }
-}
+  renderMany(images: Image[]) {
+    return images.map(image => this.render(image));
+  }
+};
